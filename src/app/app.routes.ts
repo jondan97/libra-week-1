44cc7932b44735df532ef1e28ac2552864b9a5a8
Ioannis <jondan97@gmail.com>
2023-12-18 21:17:44 +0200

    reactive forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AllUsersComponent } from './components/all-users/all-users.component';
 import { Error404Component } from './components/error404/error404.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { AllStoresComponent } from './components/all-stores/all-stores.component';
+import { CreateStoreComponent } from './components/create-store/create-store.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: "home", pathMatch: "full"},
@@ -11,5 +12,6 @@ export const routes: Routes = [
     {path: "users", component: AllUsersComponent},
     {path: "users/:id", component: UserDetailsComponent},
     {path: "stores", component: AllStoresComponent},
+    {path: "create-store", component: CreateStoreComponent},
     {path: "**", component: Error404Component}
 ];
---
+++ src/app/components/create-store/create-store.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-store',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './create-store.component.html',
+  styleUrl: './create-store.component.scss'
+})
+export class CreateStoreComponent {
+  form!: FormGroup
+
+
+  ngOnInit(){
+    this.setFormValues();
+  }
+
+  // form binding
+  setFormValues(){
+    this.form = new FormGroup({
+      firstname: new FormControl({value: "Hello", disabled: true}, Validators.required),
+      lastname: new FormControl("", Validators.required),
+      receiveNotifications: new FormControl(true),
+      stars: new FormControl("5")
+    });
+  }
+
+  onSubmit(){
+    if(this.form.valid){
+      // service backend call
+      console.log("valid!");
+    } else {
+      // error message
+      this.form.markAllAsTouched();
+      console.log("invalid");
+    }
+    console.log(this.form);
+  }
+}
